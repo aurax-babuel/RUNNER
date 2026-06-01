2ea92a5c39b5a594d6abdc541ddcd34039ec2ce4
aurax-babuel <samukadugrau1990@gmail.com>
2026-06-01 17:10:42 -0300

    Add files via upload

---
+++ menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_sair_pressed() -> void:
+	
+	pass
+
+func on_jogador_pressed() -> void:
+	
+	pass
+
+func _on_jogar_pressed() -> void:
+	pass # Replace with function body.
